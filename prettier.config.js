ae587a848ce503d2ec29c55fea20c891975b09e0
김태강 <k_2684@naver.com>
2023-09-25 16:45:22 +0900

    Setting eslint & prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindFunctions: ['clsx', 'cva'],
+};
