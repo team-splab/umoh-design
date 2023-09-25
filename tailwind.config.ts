0fa1880afdb51fe900e30ff417a4aab4b151bbe7
김태강 <k_2684@naver.com>
2023-09-25 16:45:38 +0900

    Setting tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
-import type { Config } from 'tailwindcss'
+/** @type {import('tailwindcss').Config} */
+import plugin from 'tailwindcss/plugin';
 
-const config: Config = {
+module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,13 +9,49 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        neutral : {
+          50: '#F9FAFC',
+          100: '#F2F5F7',
+          200: '#E5EAF0',
+          300: '#D9E0E8',
+          400: '#CCD5E1',
+          500: '#BFCBD9',
+          600: '#8F98A3',
+          700: '#60666D',
+          800: '#303336',
+          900: '#131416',
+        },
+        purple : {
+          50: '#F0EFFA',
+          100: '#E0DEF5',
+          200: '#C1BEEA',
+          300: '#A39DE0',
+          400: '#847DD5',
+          500: '#6056DB',
+          600: '#4C4598',
+          700: '#332E66',
+          800: '#191733',
+          900: '#0A0914',
+        },
+        primary: '#6056DB',
       },
+      textColor:{
+        main:"#131416",
+        subtle: '#303336',
+        subtlest:'#60666D',
+        inverse:'#ffffff',
+      }
     },
   },
-  plugins: [],
+    plugins: [
+      plugin(function ({ addUtilities }) {
+        addUtilities({
+          'title-lg':{
+            // TODO: add font
+          }      
+        });
+      }),
+      require('tailwindcss-animate'),
+    ],
 }
-export default config
