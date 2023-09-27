dc192432feee109924ac9816279eec56ecab4ac6
김태강 <k_2684@naver.com>
2023-09-27 17:29:07 +0900

    Implement Dark Mode Switch

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,8 @@
 import type { Preview } from '@storybook/react';
 import '../src/app/globals.css';
 
+import { withThemeByDataAttribute } from '@storybook/addon-styling';
+
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -12,5 +14,16 @@ const preview: Preview = {
     },
   },
 };
+
+export const decorators = [
+  withThemeByDataAttribute({
+    themes: {
+      light: 'light',
+      dark: 'dark',
+    },
+    defaultTheme: 'light',
+    attributeName: 'data-mode',
+  }),
+];
 
 export default preview;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 import plugin from 'tailwindcss/plugin';
-
-module.exports = {
+export default {
+  darkMode: ['class', '[data-mode="dark"]'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
