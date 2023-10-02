46a5a69f585a185618f4b925d5c090efe9dd1842
김태강 <k_2684@naver.com>
2023-10-02 14:15:04 +0900

    Implement Table Component

---
+++ src/components/Table/Table.stories.tsx
@@ -0,0 +1,69 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from './Table';
+
+const meta = {
+  title: 'Components/Table',
+  component: Table,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+    nextjs: {
+      appDirectory: true,
+    },
+    componentSubtitle: 'Table은 데이터 세트를 보여주는 반응형 컴포넌트입니다.',
+    docs: {
+      description: {
+        component:
+          '데이터를 행과 열로 구성된 Table 형식으로 표시할 수 있습니다.',
+      },
+    },
+  },
+  decorators: [
+    Story => (
+      <div className="p-12">
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof Table>;
+
+export default meta;
+
+export const DefaultTable: StoryObj<typeof Table> = {
+  parameters: {
+    docs: {
+      description: {
+        story: '기본으로 사용되는 Default Table입니다.',
+      },
+    },
+  },
+  render: args => (
+    <Table {...args}>
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Invoice</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead>Method</TableHead>
+          <TableHead className="text-right">Amount</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell className="font-medium">INV001</TableCell>
+          <TableCell>Paid</TableCell>
+          <TableCell>Credit Card</TableCell>
+          <TableCell className="text-right">$250.00</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  ),
+};
