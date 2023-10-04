b6f97b67bfe13299edcc520d21a0e840f612b019
Taekang Kim <k_2684@naver.com>
2023-10-04 18:32:28 +0900

    Add Example Calendar Form Story

--- src/components/Form/Form.stories.tsx
+++ src/components/Form/Form.stories.tsx
@@ -28,9 +28,20 @@ import {
   SelectValue,
 } from 'components/Select/Select';
 import { Textarea } from 'components/Textarea/Textarea';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from 'components/Popover/Popover';
+import { Calendar } from 'components/Calendar/Calendar';
 
 import { toast } from 'lib/useToast';
 import { Toaster } from 'components/Toast/Toaster';
+
+import { cn } from 'lib/twUtils';
+import { format } from 'date-fns';
+
+import { CalendarIcon } from 'lucide-react';
 
 const meta = {
   title: 'Components/Form',
@@ -346,7 +357,7 @@ export const WithTextareaForm: StoryObj<typeof Form> = {
     }
 
     return (
-      <Form {...form}>
+      <Form {...form} {...args}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
           className="w-2/3 space-y-6"
@@ -366,6 +377,89 @@ export const WithTextareaForm: StoryObj<typeof Form> = {
                 </FormControl>
                 <FormDescription>
                   You can <span>@mention</span> other users and organizations.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    );
+  },
+};
+
+export const WithCalendarForm: StoryObj<typeof Form> = {
+  parameters: {
+    docs: {
+      description: {
+        story: 'Calendar와 함께 사용된 Form입니다.',
+      },
+    },
+  },
+  render: args => {
+    const FormSchema = z.object({
+      dob: z.date({
+        required_error: 'A date of birth is required.',
+      }),
+    });
+    const form = useForm<z.infer<typeof FormSchema>>({
+      resolver: zodResolver(FormSchema),
+    });
+
+    function onSubmit(data: z.infer<typeof FormSchema>) {
+      toast({
+        title: 'You submitted the following values:',
+        description: (
+          <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+            <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+          </pre>
+        ),
+      });
+    }
+
+    return (
+      <Form {...form} {...args}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="dob"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel>Date of birth</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant={'outline'}
+                        className={cn(
+                          'w-[240px] pl-3 text-left font-normal',
+                          !field.value && 'text-muted-foreground'
+                        )}
+                      >
+                        {field.value ? (
+                          format(field.value, 'PPP')
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      disabled={date =>
+                        date > new Date() || date < new Date('1900-01-01')
+                      }
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+                <FormDescription>
+                  Your date of birth is used to calculate your age.
                 </FormDescription>
                 <FormMessage />
               </FormItem>
