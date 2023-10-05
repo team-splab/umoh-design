8680d2e8e126eef979e53de60f484a6488f00623
Taekang Kim <k_2684@naver.com>
2023-10-05 16:05:38 +0900

    Implement Collapsible Component

---
+++ src/components/Collapsible/Collapsible.stories.tsx
@@ -0,0 +1,127 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { useState } from 'react';
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from './Collapsible';
+import { Button } from 'components/Button/Button';
+
+import { ChevronsUpDown } from 'lucide-react';
+
+const meta = {
+  title: 'Components/Collapsible',
+  component: Collapsible,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+    nextjs: {
+      appDirectory: true,
+    },
+    componentSubtitle:
+      'Collapsible은 Panel을 확장/축소하는 Interactive 컴포넌트입니다.',
+    docs: {
+      description: {
+        component: `
+- asChild값으로 true | false 중 하나를 선택할 수 있습니다.\n
+- diabled값으로 true | false 중 하나를 선택할 수 있습니다.\n
+`,
+      },
+    },
+  },
+  argTypes: {
+    asChild: {
+      description: 'Collapsible asChild 여부를 설정합니다.',
+      table: {
+        defaultValue: { summary: 'false' },
+      },
+      control: {
+        type: 'boolean',
+      },
+    },
+    disabled: {
+      description: 'Collapsible disabled 여부를 설정합니다.',
+      table: {
+        defaultValue: { summary: 'false' },
+      },
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
+  decorators: [
+    Story => (
+      <div className="p-12">
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof Collapsible>;
+
+export default meta;
+
+export const DefaultAvatar: StoryObj<typeof Collapsible> = {
+  parameters: {
+    docs: {
+      description: {
+        story: '기본으로 사용되는 Default Collapsible입니다.',
+      },
+    },
+  },
+  render: args => (
+    <Collapsible {...args}>
+      <CollapsibleTrigger>Can I use this in my project?</CollapsibleTrigger>
+      <CollapsibleContent>
+        Yes. Free to use for personal and commercial projects. No attribution
+        required.
+      </CollapsibleContent>
+    </Collapsible>
+  ),
+};
+
+export const ExampleCollapsible: StoryObj<typeof Collapsible> = {
+  parameters: {
+    docs: {
+      description: {
+        story: 'Button과 함께 사용된 Example Collapsible입니다.',
+      },
+    },
+  },
+  render: args => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+      <Collapsible
+        open={isOpen}
+        onOpenChange={setIsOpen}
+        className="w-[350px] space-y-2"
+        {...args}
+      >
+        <div className="flex items-center justify-between space-x-4 px-4">
+          <h4 className="text-sm font-semibold">
+            @peduarte starred 3 repositories
+          </h4>
+          <CollapsibleTrigger asChild>
+            <Button variant="ghost" size="sm" className="w-9 p-0">
+              <ChevronsUpDown className="h-4 w-4" />
+              <span className="sr-only">Toggle</span>
+            </Button>
+          </CollapsibleTrigger>
+        </div>
+        <div className="rounded-md border px-4 py-3 font-mono text-sm">
+          @radix-ui/primitives
+        </div>
+        <CollapsibleContent className="space-y-2">
+          <div className="rounded-md border px-4 py-3 font-mono text-sm">
+            @radix-ui/colors
+          </div>
+          <div className="rounded-md border px-4 py-3 font-mono text-sm">
+            @stitches/react
+          </div>
+        </CollapsibleContent>
+      </Collapsible>
+    );
+  },
+};
