24a67ba9745e6569c650076bb197e693069a7059
Taekang Kim <k_2684@naver.com>
2023-10-06 12:53:12 +0900

    Implement RadioGroup Component

--- src/components/Form/Form.stories.tsx
+++ src/components/Form/Form.stories.tsx
@@ -34,6 +34,7 @@ import {
   PopoverTrigger,
 } from 'components/Popover/Popover';
 import { Calendar } from 'components/Calendar/Calendar';
+import { RadioGroup, RadioGroupItem } from 'components/RadioGroup/RadioGroup';
 
 import { toast } from 'lib/useToast';
 import { Toaster } from 'components/Toast/Toaster';
@@ -461,6 +462,88 @@ export const WithCalendarForm: StoryObj<typeof Form> = {
                 <FormDescription>
                   Your date of birth is used to calculate your age.
                 </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    );
+  },
+};
+
+export const WithRadioGroupForm: StoryObj<typeof Form> = {
+  parameters: {
+    docs: {
+      description: {
+        story: 'RadioGroup과 함께 사용된 Form입니다.',
+      },
+    },
+  },
+  render: args => {
+    const FormSchema = z.object({
+      type: z.enum(['all', 'mentions', 'none'], {
+        required_error: 'You need to select a notification type.',
+      }),
+    });
+    const form = useForm<z.infer<typeof FormSchema>>({
+      resolver: zodResolver(FormSchema),
+    });
+
+    function onSubmit(data: z.infer<typeof FormSchema>) {
+      toast({
+        title: 'You submitted the following values:',
+        description: (
+          <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+            <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+          </pre>
+        ),
+      });
+    }
+
+    return (
+      <Form {...form} {...args}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-2/3 space-y-6"
+        >
+          <FormField
+            control={form.control}
+            name="type"
+            render={({ field }) => (
+              <FormItem className="space-y-3">
+                <FormLabel>Notify me about...</FormLabel>
+                <FormControl>
+                  <RadioGroup
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    className="flex flex-col space-y-1"
+                  >
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="all" />
+                      </FormControl>
+                      <FormLabel className="font-normal">
+                        All new messages
+                      </FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="mentions" />
+                      </FormControl>
+                      <FormLabel className="font-normal">
+                        Direct messages and mentions
+                      </FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="none" />
+                      </FormControl>
+                      <FormLabel className="font-normal">Nothing</FormLabel>
+                    </FormItem>
+                  </RadioGroup>
+                </FormControl>
                 <FormMessage />
               </FormItem>
             )}
