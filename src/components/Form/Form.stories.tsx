7c8d0e49dfc4420fa74352008544c028610b4817
Taekang Kim <k_2684@naver.com>
2023-10-06 14:25:59 +0900

    Add Example Switch Form Story

--- src/components/Form/Form.stories.tsx
+++ src/components/Form/Form.stories.tsx
@@ -35,6 +35,7 @@ import {
 } from 'components/Popover/Popover';
 import { Calendar } from 'components/Calendar/Calendar';
 import { RadioGroup, RadioGroupItem } from 'components/RadioGroup/RadioGroup';
+import { Switch } from 'components/Switch/Switch';
 
 import { toast } from 'lib/useToast';
 import { Toaster } from 'components/Toast/Toaster';
@@ -548,6 +549,102 @@ export const WithRadioGroupForm: StoryObj<typeof Form> = {
               </FormItem>
             )}
           />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    );
+  },
+};
+
+export const WithSwitchForm: StoryObj<typeof Form> = {
+  parameters: {
+    docs: {
+      description: {
+        story: 'Switch와 함께 사용된 Form입니다.',
+      },
+    },
+  },
+  render: args => {
+    const FormSchema = z.object({
+      marketing_emails: z.boolean().default(false).optional(),
+      security_emails: z.boolean(),
+    });
+
+    const form = useForm<z.infer<typeof FormSchema>>({
+      resolver: zodResolver(FormSchema),
+      defaultValues: {
+        security_emails: true,
+      },
+    });
+
+    function onSubmit(data: z.infer<typeof FormSchema>) {
+      toast({
+        title: 'You submitted the following values:',
+        description: (
+          <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+            <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+          </pre>
+        ),
+      });
+    }
+
+    return (
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full space-y-6"
+        >
+          <div>
+            <h3 className="mb-4 text-lg font-medium">Email Notifications</h3>
+            <div className="space-y-4">
+              <FormField
+                control={form.control}
+                name="marketing_emails"
+                render={({ field }) => (
+                  <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
+                    <div className="space-y-0.5">
+                      <FormLabel className="text-base">
+                        Marketing emails
+                      </FormLabel>
+                      <FormDescription>
+                        Receive emails about new products, features, and more.
+                      </FormDescription>
+                    </div>
+                    <FormControl>
+                      <Switch
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="security_emails"
+                render={({ field }) => (
+                  <FormItem className="flex flex-row items-center justify-between rounded-lg border p-4">
+                    <div className="space-y-0.5">
+                      <FormLabel className="text-base">
+                        Security emails
+                      </FormLabel>
+                      <FormDescription>
+                        Receive emails about your account security.
+                      </FormDescription>
+                    </div>
+                    <FormControl>
+                      <Switch
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                        disabled
+                        aria-readonly
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+          </div>
           <Button type="submit">Submit</Button>
         </form>
       </Form>
