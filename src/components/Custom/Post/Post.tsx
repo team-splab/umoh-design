97afe86e61ace8d5afdabfbb151385fab2761e06
Taekang Kim <k_2684@naver.com>
2023-10-19 14:21:28 +0900

    Implement Responsive Post

--- src/components/Custom/Post/Post.tsx
+++ src/components/Custom/Post/Post.tsx
@@ -16,7 +16,10 @@ const PostContainer = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <AccordionPrimitive.Item
     ref={ref}
-    className={cn('flex flex-col bg-white shadow-lg', className)}
+    className={cn(
+      'flex w-56 flex-col bg-white shadow-lg data-[state=open]:w-screen md:w-80 md:data-[state=open]:w-80',
+      className
+    )}
     {...props}
   />
 ));
@@ -30,12 +33,12 @@ const PostTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        'flex flex-1 items-center justify-between border-b-2 p-4 font-medium text-primary-500 transition-all [&[data-state=open]>svg]:rotate-180',
+        'flex flex-1 items-center justify-between border-b-2 p-2 font-medium text-primary-500 transition-all md:p-4 [&[data-state=open]>svg]:rotate-180',
         className
       )}
       {...props}
     >
-      <div className="flex items-center gap-1">
+      <div className="flex items-center gap-1 text-sm md:text-base">
         <MessageSquarePlusIcon />
         Community Board
       </div>
@@ -52,7 +55,7 @@ const PostPreview = React.forwardRef<
   <AccordionPrimitive.Header
     ref={ref}
     className={cn(
-      'flex flex-col gap-1 p-4 data-[state=open]:hidden',
+      'flex max-h-16 flex-col gap-1 p-2 data-[state=open]:hidden md:max-h-32 md:p-4',
       className
     )}
     {...props}
@@ -69,7 +72,7 @@ const PostContent = React.forwardRef<
   <AccordionPrimitive.Content
     ref={ref}
     className={cn(
-      'relative flex-col overflow-hidden text-sm transition-all data-[state=closed]:h-full data-[state=open]:h-[calc(100vh-4rem)] data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down',
+      'relative flex-col overflow-hidden text-sm transition-all data-[state=closed]:h-full data-[state=open]:h-[calc(100vh-3rem)] data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down md:data-[state=open]:h-[calc(100vh-4rem)]',
       className
     )}
     {...props}
