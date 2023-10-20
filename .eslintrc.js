5be8ed7363334736d9b06f1cde0f6333d9cb4caf
Seunghyun Min <i@seunghyun.in>
2023-10-20 13:27:18 +0900

    Configure auto import sorting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,37 @@
 module.exports = {
+  plugins: ['simple-import-sort'],
   extends: ['react-app', 'plugin:storybook/recommended'],
   rules: {
     'react-hooks/rules-of-hooks': 'off',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
   },
+  overrides: [
+    {
+      files: ['**/*.js', '**/*.ts', '**/*.tsx'],
+      rules: {
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              // `react` first, `next` second, then packages starting with a character
+              ['^react$', '^next', '^[a-z]'],
+              // Packages starting with `@`
+              ['^@'],
+              // Packages starting with `~`
+              ['^~'],
+              // Imports starting with `../`
+              ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+              // Imports starting with `./`
+              ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+              // Style imports
+              ['^.+\\.s?css$'],
+              // Side effect imports
+              ['^\\u0000'],
+            ],
+          },
+        ],
+      },
+    },
+  ],
 };
