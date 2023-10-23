b3f206a5321af56b3493b817cfb3a757b4c2fcf7
Taekang Kim <k_2684@naver.com>
2023-10-23 18:11:18 +0900

    Fix import pluginConfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+
+import pluginConfig from './src/lib/plugin';
+
 const config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-  plugins: [require('tailwindcss-animate'), require('./src/lib/plugin')],
+  plugins: [require('tailwindcss-animate'), pluginConfig],
 };
 
 export default config;
