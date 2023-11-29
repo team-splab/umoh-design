fa734fc74d523dfcc49ef3e90ef2c1d9651a18fe
Taekang Kim <k_2684@naver.com>
2023-11-29 18:48:21 +0900

    Add Storybook manager and theme configuration

---
+++ .storybook/theme.ts
@@ -0,0 +1,11 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'dark',
+
+  brandTitle: 'Classcope',
+  brandImage: 'https://storage.umoh.io/images/logos/umoh_logo_purple.png',
+
+  colorPrimary: '#7433FF',
+  colorSecondary: '#D3BEFF',
+});
