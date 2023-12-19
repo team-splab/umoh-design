d6fe02e9959e03bd5b2ef6e7565da6d32b7ea570
Grayash <k_2684@naver.com>
2023-12-19 15:58:17 +0900

    Remove close button from Sheet component

--- src/components/Base/Sheet/Sheet.tsx
+++ src/components/Base/Sheet/Sheet.tsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from 'lib/twUtils';
-import { X } from 'lucide-react';
 
 import * as SheetPrimitive from '@radix-ui/react-dialog';
 
@@ -72,10 +71,6 @@ const SheetContent = React.forwardRef<
       {...props}
     >
       {children}
-      <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-gray-100">
-        <X className="h-4 w-4" />
-        <span className="sr-only">Close</span>
-      </SheetPrimitive.Close>
     </SheetPrimitive.Content>
   </SheetPortal>
 ));
