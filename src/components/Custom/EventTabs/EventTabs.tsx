0552697a0613ab204dafa0cbf6f884ca1333fde8
Grayash <k_2684@naver.com>
2023-12-19 21:33:57 +0900

    Update styling in EventTabs component

--- src/components/Custom/EventTabs/EventTabs.tsx
+++ src/components/Custom/EventTabs/EventTabs.tsx
@@ -14,7 +14,7 @@ const EventTabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      'inline-flex w-full items-center justify-between px-6 py-1.5 text-gray-500 tablet:py-0',
+      'inline-flex w-full items-center justify-between py-1.5 text-gray-500 tablet:py-0',
       className
     )}
     {...props}
@@ -29,7 +29,7 @@ const EventTabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      'inline-flex h-full w-full flex-col items-center justify-center whitespace-nowrap rounded-none border-b border-transparent bg-white p-0 px-4 py-2 text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 data-[state=active]:border-black data-[state=active]:text-gray-950',
+      'inline-flex h-full w-full flex-col items-center justify-center whitespace-nowrap rounded-none border-b border-transparent bg-white p-0 px-3 py-2 text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 data-[state=active]:border-black data-[state=active]:text-gray-950 tablet:px-4',
       className
     )}
     {...props}
