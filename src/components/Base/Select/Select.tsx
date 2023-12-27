b8df97630b9b15434f26340cc5d4c02def9c86cc
Seunghyun Min <i@seunghyun.in>
2023-12-27 15:12:11 +0900

    Remove ring effect from Select

--- src/components/Base/Select/Select.tsx
+++ src/components/Base/Select/Select.tsx
@@ -19,7 +19,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      'flex h-10 w-full items-center justify-between rounded-md border border-gray-200 bg-white px-3 py-2 text-sm ring-offset-white placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+      'flex h-10 w-full items-center justify-between rounded-md border border-gray-200 bg-white px-3 py-2 text-sm placeholder:text-gray-500 focus:outline-none disabled:cursor-not-allowed disabled:opacity-50',
       className
     )}
     {...props}
